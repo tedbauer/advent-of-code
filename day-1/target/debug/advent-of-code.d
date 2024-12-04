a7041a0f16c29b2c33b5686e6fb2ba78be8a7bfe
tedbauer <tjb272@cornell.edu>
2024-12-03 22:48:44 -0500

    New dir structure

---
+++ day-1/target/debug/advent-of-code.d
@@ -0,0 +1,1 @@
+/Users/theodorebauer/Documents/advent-of-code/day-1/target/debug/advent-of-code: /Users/theodorebauer/Documents/advent-of-code/day-1/src/main.rs
---
+++ day-1/target/debug/deps/advent_of_code-77410501be6b6b9b.d
@@ -0,0 +1,5 @@
+/Users/theodorebauer/Documents/advent-of-code/day-1/target/debug/deps/advent_of_code-77410501be6b6b9b: src/main.rs
+
+/Users/theodorebauer/Documents/advent-of-code/day-1/target/debug/deps/advent_of_code-77410501be6b6b9b.d: src/main.rs
+
+src/main.rs:
